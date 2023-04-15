15f1a91dc3c295b349a3a26e1523cbe19c32f581
jayantmukundam <jayantmukundam2811@gmail.com>
2023-04-15 13:35:06 +0530

    delete button for hobbies

--- server/src/index.js
+++ server/src/index.js
@@ -13,8 +13,9 @@ app.use('/auth', userRouter)
 app.use('/events', eventsRouter)
 app.use('/hobbies',hobbiesRouter)
 
-mongoose.connect(
-  'mongodb+srv://jayparekh:rock123A@cluster0.awazspn.mongodb.net/Cluster0?retryWrites=true&w=majority'
-)
+// mongoose.connect(
+//   'mongodb+srv://jayparekh:rock123A@cluster0.awazspn.mongodb.net/Cluster0?retryWrites=true&w=majority'
+// )
+mongoose.connect("mongodb+srv://jayparekh:rock123A@production.074bxra.mongodb.net/?retryWrites=true&w=majority")
 
 app.listen(3001, () => console.log('Server Started'))
--- server/src/routes/hobbies.js
+++ server/src/routes/hobbies.js
@@ -52,4 +52,28 @@ router.get('/', async (req, res) => {
     }
   })
 
+router.put('/:hobbyID/user/:userID', async (req, res) => {
+  try {
+    const hobby = await HobbyModel.findById(req.params.hobbyID)
+    const user = await UserModel.findById(req.params.userID)
+    // console.log("hobby.hobbyName")
+    const userIndex = hobby.userList.indexOf(req.params.userID);
+    const hobbyIndex = user.hobbies.indexOf(req.params.hobbyID);
+    console.log(userIndex)
+    console.log(hobbyIndex)
+
+    if (userIndex > -1) { // only splice array when item is found
+      hobby.userList.splice(userIndex, 1); // 2nd parameter means remove one item only
+    }
+    if(hobbyIndex>-1){
+      user.hobbies.splice(hobbyIndex,1);
+    }
+    await hobby.save()
+    await user.save()
+    res.json(hobbies)
+  } catch (err) {
+    res.json(err)
+  }
+})
+
 export { router as hobbiesRouter }
