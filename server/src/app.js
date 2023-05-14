ab859dec35f31593452144d0ef47fbb6a6dedcd9
jayantmukundam <jayantmukundam2811@gmail.com>
2023-05-14 17:37:05 +0530

    testing pipeline

--- server/src/app.js
+++ server/src/app.js
@@ -32,7 +32,7 @@ const logger = winston.createLogger({
       level: 'info',
       index: 'logs',
       clientOpts: {
-        node: 'http://08f2-103-156-19-229.ngrok-free.app',
+        node: 'http://e885-103-156-19-229.ngrok-free.app',
       },
     }),
   ],
