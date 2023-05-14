cbc2ca8870e11b0fc5b9cd6dea418f6d8c55967a
Jay Parekh <jay.parekh@iiitb.ac.in>
2023-05-14 14:56:16 +0530

    BUG FIX

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,7 @@
 
 import app from './app.js'
 import config from '../utils/config.js'
+const emitter = new EventEmitter()
+emitter.setMaxListeners(100)
 
 app.listen(config.PORT, () => console.log('Server Started'))
