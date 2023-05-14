66dc1fe23801f1cb7d479dbbe363d04f5aa29d79
Jay Parekh <jay.parekh@iiitb.ac.in>
2023-05-14 12:46:34 +0530

    Bug fix

--- server/utils/config.js
+++ server/utils/config.js
@@ -22,14 +22,14 @@ let env = null
 NEW CONFIG FOR K8s DEPLOYMENT
 */
 env = process.env
-
+let process_env = process.env.NODE_ENV
 // const PORT = 3001
 // const MONGODB_URI =
 //   'mongodb+srv://jayparekh:rock123A@production.074bxra.mongodb.net/?retryWrites=true&w=majority'
 // const SECRET = env.SECRET
 let MONGODB_URI
 const PORT = env.PORT
-if(env=="production") {
+if(process_env=="production") {
     MONGODB_URI = env.MONGODB_URI_PRODUCTION
 }
 else{
