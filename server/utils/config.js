c290e0c3b4bf0cac10442ef3e4eff4fb5456245f
jayparekh691 <120568495+jayparekh691@users.noreply.github.com>
2023-05-15 20:34:55 +0530

    Update config.js

--- server/utils/config.js
+++ server/utils/config.js
@@ -1,26 +1,5 @@
-// const yenv = require('yenv')
-// require('dotenv').config()
 let env = null
 
-/*
- * OLD ORIGINAL CONFIG
- * If production environment then use the env.yaml file (generated via Ansible Vault + Playbook)
- * for the environment variables.
- * If testing environment then use the env-enc.yaml file (which will be decrypted in the Jenkins pipeline).
- * And if in development environment, use the env-local.yaml file.
- */
-// if (process.env.NODE_ENV == 'production')
-// 	env = yenv()
-// else if (process.env.NODE_ENV == 'test')
-// 	env = yenv('env-enc.yaml')
-// else if (process.env.NODE_ENV == 'production-heroku')
-// 	env = process.env
-// else
-// 	env = yenv('env-local.yaml')
-
-/*
-NEW CONFIG FOR K8s DEPLOYMENT
-*/
 env = process.env
 
 // const PORT = 3001
