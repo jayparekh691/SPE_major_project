670cd268ff3029f04d9025fb5038510226c2fbb5
jayparekh691 <120568495+jayparekh691@users.noreply.github.com>
2024-04-29 07:21:31 +0530

    Update config.js

--- server/utils/config.js
+++ server/utils/config.js
@@ -2,10 +2,7 @@ let env = null
 
 env = process.env
 
-// const PORT = 3001
-// const MONGODB_URI =
-//   'mongodb+srv://jayparekh:rock123A@production.074bxra.mongodb.net/?retryWrites=true&w=majority'
-// const SECRET = env.SECRET
+
 
 let MONGODB_URI
 const PORT = env.PORT
